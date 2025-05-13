4a0d69174bd3ddb3ffd246dd0ba4688cb3f5b617
umarzamann <beingumar9@gmail.com>
2025-05-13 16:03:19 +0500

    showing error toast for duplicate item code on INSERT

--- inventory_insert.php
+++ inventory_insert.php
@@ -8,7 +8,12 @@ $item_price = $_POST['price'];
 $item_stock = $_POST['stock'];
 $item_description = $_POST['description'];
 
+$check_query = "SELECT * FROM items WHERE sku = '{$item_code}' LIMIT 1";
+$check_item_code = mysqli_query($conn, $check_query) or die("Check Query Failed");
 
+if (mysqli_num_rows($check_item_code) > 0) {
+    echo "Item code is already in use";
+} else {
     $sql = "INSERT INTO items(item_name,sku,price,stock,`description`) 
             VALUES ('{$item_name}','{$item_code}','{$item_price}','{$item_stock}','{$item_description}')";
     $result = mysqli_query($conn, $sql) or die("Query Unsuccessful");
@@ -19,7 +24,7 @@ $item_description = $_POST['description'];
     } else {
         die("Something went wrong try again: " . mysqli_error($conn));
     }
-
+}
 
 // header("Location: http://localhost/shopmate_org_pagination/view_users.php");
 
